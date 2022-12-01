71ef4b74909994dab82b53d22a8d90bf8ba9cf87
Juan Garcia <juangar04@gmail.com>
2022-12-01 20:11:42 +0100

    Primer commit

---
+++ Invertir.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios;
+
+/**
+ *
+ * @author gargarju
+ */
+public class Invertir {
+    public static void main(String[] args) {
+        
+    }
+}
